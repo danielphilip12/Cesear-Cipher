a35fda53034ea1fb54484d8c8f5519c134653f5e
Hawkeye312 <daniel.gallo12@gmail.com>
2019-01-31 10:38:26 -0800

    Created Encrypt Function

--- Cesaer.py
+++ Cesaer.py
@@ -1,7 +1,19 @@
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-key = 5
-letter = input("What letter: ")
-#A comment for testing
-x = alphabet.index(letter)
-new_letter_loc = (x + key) % 26
-print(alphabet[new_letter_loc])
+
+def encrypt(string, key):
+    string = string.lower()
+    new_string = ""
+    for i in string:
+        if(i in alphabet):
+            x = alphabet.index(i)
+            new_letter_loc = (x + key) % 26
+            new_string += alphabet[new_letter_loc]
+        else:
+            new_string += i
+
+    return new_string
+
+x = "Hello World"
+encrypted_x = encrypt(x, 7)
+print(x)
+print(encrypted_x)
