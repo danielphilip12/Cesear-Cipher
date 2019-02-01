5faf56da616cda776c643936d704927a58d49561
Hawkeye312 <daniel.gallo12@gmail.com>
2019-02-01 10:56:16 -0800

    Encrypt and Decrypt now take capital letters

--- Cesear.py
+++ Cesear.py
@@ -1,14 +1,18 @@
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-
+alpha_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 def encrypt(string, key):
-    string = string.lower()
+    #string = string.lower()
     new_string = ""
     for i in string:
         if i in alphabet:
             x = alphabet.index(i)
             new_letter_loc = (x + key) % 26
             new_string += alphabet[new_letter_loc]
+        elif i in alpha_alphabet:
+            x = alpha_alphabet.index(i)
+            new_letter_loc = (x + key) % 26
+            new_string += alpha_alphabet[new_letter_loc]
         else:
             new_string += i
 
@@ -16,21 +20,26 @@ def encrypt(string, key):
 
 
 def decrypt(string, key):
-    string = string.lower()
+    #string = string.lower()
     new_string = ""
     for i in string:
         if i in alphabet:
             x = alphabet.index(i)
             new_letter_loc = (x - key) % 26
             new_string += alphabet[new_letter_loc]
+        elif i in alpha_alphabet:
+            x = alpha_alphabet.index(i)
+            new_letter_loc = (x - key) % 26
+            new_string += alpha_alphabet[new_letter_loc]
         else:
             new_string += i
 
     return new_string
 
 
-x = input("Enter a string: ")
-key = int(input("Enter a number between 1-26: "))
-new_x = encrypt(x, key)
-print(x)
-print(new_x)
+# word = "Hello World"
+# en = encrypt(word, 7)
+# de = decrypt(en, 7)
+# print(word)
+# print(en)
+# print(de)
--- Cipher_Test.py
+++ Cipher_Test.py
@@ -5,18 +5,18 @@ import Cesear
 class TestCesearCipher(unittest.TestCase):
 
     def test_encrypt(self):
-        self.assertEqual(Cesear.encrypt("Hello world", 7), "olssv dvysk")
+        self.assertEqual(Cesear.encrypt("Hello world", 7), "Olssv dvysk")
 
-        self.assertEqual(Cesear.encrypt("Daniel", 1), "ebojfm")
+        self.assertEqual(Cesear.encrypt("Daniel", 1), "Ebojfm")
 
         self.assertEqual(Cesear.encrypt("abc", 23), "xyz")
 
-        self.assertEqual(Cesear.encrypt("Hello", 26), "hello")
+        self.assertEqual(Cesear.encrypt("Hello", 26), "Hello")
 
     def test_decrypt(self):
         x = Cesear.encrypt("Daniel", 1)
         y = Cesear.encrypt("Hello world", 7)
         z = Cesear.encrypt("abc", 23)
-        self.assertEqual(Cesear.decrypt(x, 1), "daniel")
-        self.assertEqual(Cesear.decrypt(y, 7), "hello world")
+        self.assertEqual(Cesear.decrypt(x, 1), "Daniel")
+        self.assertEqual(Cesear.decrypt(y, 7), "Hello world")
         self.assertEqual(Cesear.decrypt(z, 23), "abc")
