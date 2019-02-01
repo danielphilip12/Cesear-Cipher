6d4d2a1e0fafc17c7b4c608b8c7009d04a990b66
Hawkeye312 <daniel.gallo12@gmail.com>
2019-02-01 12:25:26 -0800

    Edited the unit tests

--- Cipher_Test.py
+++ Cipher_Test.py
@@ -14,7 +14,7 @@ class TestCesearCipher(unittest.TestCase):
         self.assertEqual(Cesear.encrypt("Hello", 26), "Hello")
 
     def test_decrypt(self):
-        x = Cesear.encrypt("Daniel", 1)
+        x = Cesear.encrypt("Your Name", 1)
         y = Cesear.encrypt("Hello world", 7)
         z = Cesear.encrypt("abc", 23)
         self.assertEqual(Cesear.decrypt(x, 1), "Your Name")
